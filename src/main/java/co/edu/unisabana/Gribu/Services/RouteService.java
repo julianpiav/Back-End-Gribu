801e6aa0b340894f69544826d2cbf48ad90f60e7
JCDuranV <jcduranv0708@gmail.com>
2023-10-20 09:19:15 -0500

    creacion servicios de rutas y etiquetas

--- src/main/java/co/edu/unisabana/Gribu/Services/RouteService.java
+++ src/main/java/co/edu/unisabana/Gribu/Services/RouteService.java
@@ -1,8 +1,25 @@
 package co.edu.unisabana.Gribu.Services;
 
+import co.edu.unisabana.Gribu.Entities.Route;
+import co.edu.unisabana.Gribu.Repositories.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RouteService {
+    @Autowired
+    RouteRepository routeRepository;
 
+    public List<Route> getRoutes() {
+        return routeRepository.findAll();
+    }
+
+    public Optional<Route> getRoute(Long id) { return routeRepository.findById(id); }
+
+    public void saveOrUpdate(Route route) {
+        routeRepository.save(route);
+    }
+    public void delete(Long id) { routeRepository.deleteById(id); }
 }
